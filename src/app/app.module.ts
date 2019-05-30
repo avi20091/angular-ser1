562b2ff1bd8fe0c0500110255275524bcbf7e1fb
avi20091 <avi20091@gmail.com>
2019-05-30 13:45:13 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { CountriesService } from './countries.service';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CountriesComponent } from './countries.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
+  declarations: [ AppComponent, HelloComponent, CountriesComponent ],
+  providers:[CountriesService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/countries.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CountriesService } from './countries.service';
+import { HttpErrorResponse } from '@angular/common/http';
+@Component({
+  selector: 'country',
+  templateUrl: './countries.component.html'
+})
+export class CountriesComponent{
+  private result:any;
+  constructor(private _service:CountriesService){};
+  ngOnInit(){
+    this._service.getCountries().subscribe(this._successCallBack,this._errorCallBack);
+  }
+  public _successCallBack=(res)=>{
+    this.result=res;
+  }
+  public _errorCallBack=(err:HttpErrorResponse)=>{
+    if(err.error instanceof Error){
+      console.log("Client Side Error....!!!!");
+    }else{
+      console.log("Server Side Error.....!!!!");
+    }
+  }
+}
